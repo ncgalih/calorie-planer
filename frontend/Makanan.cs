d508814a95a7b90f873864c52bb7f84a0b5aa644
Nc Galih <encegalih@gmail.com>
2023-11-15 14:41:16 +0700

    food API

--- frontend/Makanan.cs
+++ frontend/Makanan.cs
@@ -5,28 +5,28 @@ using System;
 public class Makanan
 {
     private string _foodName;
-    private int _calorie;
-    private int _carbohydrate;
-    private int _fat;
+    private float _calorie;
+    private float _carbohydrate;
+    private float _fat;
 
     public string FoodName
     {
         get { return _foodName; }
     }
 
-    public int Calorie
+    public float Calorie
     {
         get { return _calorie; }
         set { _calorie = value; }
     }
 
-    public int Carbohydrate
+    public float Carbohydrate
     {
         get { return _carbohydrate; }
         set { _carbohydrate = value; }
     }
 
-    public int Fat
+    public float Fat
     {
         get { return _fat; }
         set { _fat = value; }
